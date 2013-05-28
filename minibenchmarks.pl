e00a84431009fb57f73362b104e4a5bae3bacfd6
Geoffrey Broadwell <gjb@sonic.net>
2013-05-28 02:00:45 -0700

    Set larger minimum scales to avoid noise at left of plots when compile time is ignored, potentially preventing nonsense performance ratios

--- minibenchmarks.pl
+++ minibenchmarks.pl
@@ -2,6 +2,7 @@
  {
   name  => 'rc-forest-fire',
   skip  => [qw( niecza.nqp p6.pl p6.js_v8 )],
+  scale => 1 << 3,
   x_label => 'Frames per Run',
   y_label => 'Frames per Second',
   perl5 => [qw( BENCH/perl5/rc-forest-fire 16 16 SCALE )],
