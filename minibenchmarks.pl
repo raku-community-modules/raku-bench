946f2d08eadb940a206d73012a4474c04f5a9001
Geoffrey Broadwell <gjb@sonic.net>
2013-05-28 01:05:58 -0700

    Support axis labels for plots, including custom labels for rc-forest-fire and rc-man-or-boy-test

--- minibenchmarks.pl
+++ minibenchmarks.pl
@@ -2,6 +2,8 @@
  {
   name  => 'rc-forest-fire',
   skip  => [qw( niecza.nqp p6.pl p6.js_v8 )],
+  x_label => 'Frames per Run',
+  y_label => 'Frames per Second',
   perl5 => [qw( BENCH/perl5/rc-forest-fire 16 16 SCALE )],
   perl6 => [qw( BENCH/perl6/rc-forest-fire 16 16 SCALE )],
   nqp   => [qw( BENCH/nqp/rc-forest-fire   16 16 SCALE )],
@@ -11,6 +13,7 @@
   skip  => [qw( )],
   scale => 10,
   scaling => 'linear',
+  x_label => 'k Parameter',
   perl5 => [qw( BENCH/perl5/rc-man-or-boy-test SCALE )],
   perl6 => [qw( BENCH/perl6/rc-man-or-boy-test SCALE )],
   nqp   => [qw( BENCH/nqp/rc-man-or-boy-test   SCALE )],
